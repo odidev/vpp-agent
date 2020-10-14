37b9984b6d305bfc6f83bd826140092f5c9fc552
odidev <odidev@puresoftware.com>
2020-10-14 12:33:16 +0530

    Update push_image.sh

--- docker/prod/push_image.sh
+++ docker/prod/push_image.sh
@@ -8,4 +8,4 @@
 # Warning: use only IMMEDIATELY after docker/dev/build.sh to prevent INCONSISTENCIES such as 
 #          a) after building image you switch to other branch which will result in mismatch of version of image and its tag
 #          b) you do not build the new image but only simply run this script which will result in mismatch version of image and its tag because the image is older than repository 
-LOCAL_IMAGE='prod_vpp_agent:latest' IMAGE_NAME='vpp-agent' ../dev/push_image.sh
+LOCAL_IMAGE='prod_vpp_agent_arm64:latest' IMAGE_NAME='vpp-agent-arm64' ../dev/push_image.sh
